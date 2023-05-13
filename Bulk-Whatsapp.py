6c6176eda1ac0678fe8312bab87b9959d8da655a
Piyush Yadav <93871548+PiyushYadav007@users.noreply.github.com>
2023-05-13 12:35:01 +0530

    Create Bulk-Whatsapp.py
    Send Whatsapp messages in bulk

---
+++ Bulk-Whatsapp.py
@@ -0,0 +1,23 @@
+import pywhatkit
+
+L=[]
+
+def whatsapp_message():
+    i = int(input("Enter the total number of contacts you want to send message : "))
+    for i in range(0,i):
+       count_code = input("Enter the country code (Start with +) : ")
+       number = input("Enter the number you want to send message : ")
+       m_no = count_code + number
+       L.append(m_no)
+    
+    message = input("Enter the message you want to send : ")
+    hour = int(input("Enter the time in hour : "))
+    minute = int(input("Enter the time in minute : "))
+    
+    for k in range(0,len(L)):
+        pywhatkit.sendwhatmsg(L[k],message,)
+        print("Message Sent")
+    
+
+while True:
+ whatsapp_message()
